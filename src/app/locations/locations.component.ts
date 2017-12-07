97c9ef93d6b8009ec0f97172e7add076bceeb2c2
unknown <wesleyy.liao@gmail.com>
2017-12-07 17:25:35 -0500

    Moved location logic to options. Locations now consists only of map and lat/lon

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '../models/location';
 import { LocationService } from '../services/location.service';
-import { Options } from '../models/options';
 
 declare var ol: any;
 
@@ -22,27 +21,26 @@ export class LocationsComponent implements OnInit {
   endLatitude: number;
   endLongitude: number;
 
-  options: Options;
-  maximizeElevation: boolean;
-  minimizeElevation: boolean;
-  limit: number;
-
-  submit() {
-    this.loadLocation(this.startLatitude, this.startLongitude, this.startLocation);
-    this.loadLocation(this.endLatitude, this.endLongitude, this.endLocation);
-    console.log('start loc: ' + this.startLocation);
-    console.log('end loc: ' + this.endLocation);
-    console.log("Maximize: " + this.maximizeElevation);
-    console.log("Minimize: " + this.minimizeElevation);
-    console.log("Limit: " + this.limit);
-  }
-
-  // needs rewriting
-  loadLocation(latitude, longitude, location) {
-    this.locationService.getLocation(latitude, longitude).subscribe(data => location = data);
-  }
-
   ngOnInit() {
+/*
+    var coordinates = [
+      new ol.geom.Point([-72.60726928710939, 42.19717693341468]),
+      new ol.geom.Point([-72.61241912841795, 42.181007970107316]),
+      new ol.geom.Point([-72.56298065185545, 42.14639853939718]),
+      new ol.geom.Point([-72.44075775146483, 42.163705621262466]),
+      new ol.geom.Point([-72.40917205810547, 42.15556170171246]),
+    ];
+    var routeGeom = new ol.geom.LineString(coordinates).transform('EPSG:4326','EPSG:3857');
+    var routeFeature = new ol.Feature({geometry:routeGeom});
+    var extentToZoom = routeGeom.getExtent();
+    console.log(extentToZoom);
+    console.log(routeFeature);
+    var routeVectorSource = new ol.source.Vector({
+      features: [routeFeature]
+    });
+    var routeVectorLayer = new ol.layer.Vector({
+      source: routeVectorSource
+    });*/
 
     var startVectorSource = new ol.source.Vector();
     var startVectorLayer = new ol.layer.Vector({
@@ -63,42 +61,46 @@ export class LocationsComponent implements OnInit {
           })
         }),
         startVectorLayer
+      //  routeVectorLayer
       ],
       view: new ol.View({
         center: ol.proj.fromLonLat([-72.49, 42.25]),//ol.proj.transform([-432.559, 42.36], 'EPSG:4326', 'EPSG:3857'),
         zoom: 9
       })
     });
+
+  //  startMap.getView().fit(extentToZoom,startMap.getSize())
+
     var endMap = new ol.Map({
-          target: 'endMap',
-          layers: [
-            new ol.layer.Tile({
-              source: new ol.source.OSM({
-                url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-              })
-            }),
-            endVectorLayer
-          ],
-          view: new ol.View({
-            center: ol.proj.fromLonLat([-72.49, 42.25]);//ol.proj.transform([-432.559, 42.36], 'EPSG:4326', 'EPSG:3857'),
-            zoom: 9
+      target: 'endMap',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           })
+        }),
+        endVectorLayer
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([-72.49, 42.25]),//ol.proj.transform([-432.559, 42.36], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 9
+      })
     });
     var iconStyle = new ol.style.Style({
-    image: new ol.style.Icon({
-        anchor: [0.5, 46],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'pixels',
-        opacity: 0.75,
-        src: '//openlayers.org/en/v3.8.2/examples/data/icon.png'
-    }),
-    text: new ol.style.Text({
-        font: '12px Calibri,sans-serif',
-        fill: new ol.style.Fill({ color: '#000' }),
-        stroke: new ol.style.Stroke({
-            color: '#fff', width: 2
-        }),
-    })
+      image: new ol.style.Icon({
+          anchor: [0.5, 46],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          opacity: 0.75,
+          src: '//openlayers.org/en/v3.8.2/examples/data/icon.png'
+      }),
+      text: new ol.style.Text({
+          font: '12px Calibri,sans-serif',
+          fill: new ol.style.Fill({ color: '#000' }),
+          stroke: new ol.style.Stroke({
+              color: '#fff', width: 2
+          }),
+      })
     });
     startMap.on('click', function(e) {
       var lonlat = ol.proj.toLonLat(e.coordinate, new ol.proj.Projection({code: 'EPSG:3857'}));
--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Options } from '../models/options';
+import { Location } from '../models/location';
+import { LocationService } from '../services/location.service';
 
 @Component({
   selector: 'app-options',
@@ -8,8 +10,51 @@ import { Options } from '../models/options';
 })
 export class OptionsComponent implements OnInit {
 
+  constructor(private locationService: LocationService) { }
+
+  startLocation: Location;
+  @Input() startLongitude: number;
+  @Input() startLatitude: number;
+
+  endLocation: Location;
+  @Input() endLongitude: number;
+  @Input() endLatitude: number;
+
+  options: Options;
+  maximizeElevation: boolean;
+  minimizeElevation: boolean;
+  limit: number;
+
   ngOnInit() {
+    this.maximizeElevation = false;
+    this.minimizeElevation = false;
+    this.limit = 0;
     //init logic
+  }
+
+  elevationClick(val){
+    if(this.maximizeElevation && this.minimizeElevation){
+      if(val=="maximize"){
+        this.minimizeElevation = false;
+      } else {
+        this.maximizeElevation = false;
+      }
+    }
+  }
+  submit() {
+    this.loadLocation(this.startLatitude, this.startLongitude, this.startLocation);
+    this.loadLocation(this.endLatitude, this.endLongitude, this.endLocation);
+    console.log('start loc: ' + this.startLocation);
+    console.log('end loc: ' + this.endLocation);
+    console.log("Maximize: " + this.maximizeElevation);
+    console.log("Minimize: " + this.minimizeElevation);
+    console.log("Limit: " + this.limit);
+  }
+
+  // needs rewriting
+  loadLocation(latitude, longitude, location) {
+    this.locationService.getLocation(latitude, longitude).subscribe(data => location = data);
+    console.log(this.locationService.getLocation(latitude, longitude));
   }
 
 }
