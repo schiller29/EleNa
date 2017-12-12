975771049918242396b5f172f97854093a981042
Mike Schiller <mschiller@mschiller.local>
2017-12-12 00:01:14 -0500

    Implemented error messages for the new UI design

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -30,6 +30,8 @@ export class OptionsComponent implements OnInit {
   endLatitudeError: boolean = false;
   endLongitudeError: boolean = false;
 
+  errorMessage : string = "";
+
   buttonText: string;
   mapRoute: boolean;
 
@@ -52,32 +54,44 @@ export class OptionsComponent implements OnInit {
   }
 
   submit() {
+    this.startLatitudeError = false;
+    this.startLongitudeError = false;
+    this.endLatitudeError = false;
+    this.endLongitudeError = false;
     if (!this.checkInputError()){
       this.loadLocations(this.startLatitude, this.startLongitude, this.endLatitude, this.endLongitude);
     } else {
       this.mapRoute = false;
       this.buttonText = 'Route';
     }
+    console.log(this.endLatitudeError);
   }
 
   checkInputError(){
     var inputError = false;
-    if(isNaN(this.startLatitude)){
+    var errorArray = [];
+    //just going to assume for simplicity's sake that is an invalid lat + long
+    if(isNaN(this.startLatitude) || this.startLatitude == 0){
       this.startLatitudeError = true;
       inputError = true;
+      errorArray.push("Start Latitude");
     }
-    if(isNaN(this.startLongitude)){
+    if(isNaN(this.startLongitude) || this.startLongitude == 0){
       this.startLongitudeError = true;
       inputError = true;
+      errorArray.push("Start Longitude");
     }
-    if(isNaN(this.endLatitude)){
+    if(isNaN(this.endLatitude) || this.endLatitude == 0){
       this.endLatitudeError = true;
       inputError = true;
+      errorArray.push("End Latitude");
     }
-    if(isNaN(this.endLongitude)){
+    if(isNaN(this.endLongitude) || this.endLongitude == 0){
       this.endLongitudeError = true;
       inputError = true;
+      errorArray.push("End Longitude");
     }
+    this.errorMessage = "Please enter valid: " + errorArray.join(", ");
     return inputError;
   }
   
