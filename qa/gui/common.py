7ac4a628a17716fb40db859ce9184edd0ce85113
Mike Schiller <mschiller@mschiller.local>
2017-12-12 02:01:04 -0500

    test_valid [ironic] does not work, because the selenium elements do not hold the text actually in the forms. Need to fix before submission

--- qa/gui/common.py
+++ qa/gui/common.py
@@ -1,6 +1,5 @@
 import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from time import sleep
 
 directory = os.getcwd()
 chromedriver = directory + '/chromedriver'
@@ -11,3 +10,21 @@ os.environ["webdriver.chrome.driver"] = chromedriver
 
 def get_elem_by_selector(elena, selector):
     return elena.find_element_by_css_selector(selector)
+
+
+def wait_for_elem(elem, appear=True, timeout=3):
+    times = 0
+    while times < timeout:
+        # Forces the tests to wait 1.5 seconds
+        sleep(1.5)
+        if appear and elem.is_displayed():
+            return True
+        elif appear:
+                pass
+        elif elem.is_displayed():
+            pass
+        else:
+            return True
+        times += 1
+
+    raise ValueError('The element did not perform as expected in the alloted time')
--- qa/gui/test_ui_validation.py
+++ qa/gui/test_ui_validation.py
@@ -1,4 +1,7 @@
-from common import chromedriver, webdriver, get_elem_by_selector
+from common import (chromedriver,
+                    get_elem_by_selector,
+                    wait_for_elem)
+from selenium import webdriver
 import unittest
 
 
@@ -10,22 +13,26 @@ class TestStringMethods(unittest.TestCase):
     def setUpClass(cls):
         cls.elena = webdriver.Chrome(chromedriver)
         cls.elena.get('http://localhost:4200/')
+        cls.body = get_elem_by_selector(cls.elena, 'body')
         cls.start_lat = get_elem_by_selector(cls.elena, '#startLatitude')
         cls.start_lon = get_elem_by_selector(cls.elena, '#startLongitude')
         cls.end_lat = get_elem_by_selector(cls.elena, '#endLatitude')
         cls.end_lon = get_elem_by_selector(cls.elena, '#endLongitude')
         cls.modal = get_elem_by_selector(cls.elena, '#modal')
-        cls.submit = get_elem_by_selector(cls.elena, '#route')
+        cls.submit_button = get_elem_by_selector(cls.elena, '#route')
 
     @classmethod
     def tearDownClass(cls):
         cls.elena.close()
 
     def tearDown(self):
-        self.start_lat.send_keys('')
-        self.start_lon.send_keys('')
-        self.end_lat.send_keys('')
-        self.end_lon.send_keys('')
+        self.empty_text_fields()
+
+    def empty_text_fields(self):
+        self.start_lat.clear()
+        self.start_lon.clear()
+        self.end_lat.clear()
+        self.end_lon.clear()
 
     def scroll_to_bottom(self, top=False):
         if top:
@@ -33,14 +40,50 @@ class TestStringMethods(unittest.TestCase):
         else:
             self.elena.execute_script("window.scrollTo(0, " +
                                       "document.body.scrollHeight);")
+
+    def exit_modal(self):
+        get_elem_by_selector(self.elena, '#modal > div').click()
+        wait_for_elem(self.body, appear=True)
+
+    def submit(self):
+        self.scroll_to_bottom()
+        self.submit_button.click()
+
     """
     Test Cases
     """
 
     def test_empty(self):
-        self.scroll_to_bottom()
-        self.submit.click()
+        self.submit()
         self.assertTrue(self.modal.is_displayed())
+        self.exit_modal()
+
+    def test_one_empty(self):
+        self.start_lat.send_keys('20.0')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('30.2')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+
+    def test_two_empty(self):
+        self.start_lat.send_keys('20.0')
+        self.end_lon.send_keys('30.2')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+
+    def test_valid(self):
+        self.start_lat.send_keys('20.0')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('30.2')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        # TODO these assertions do not work
+        self.assertEqual(self.start_lat.text, '20.0')
+        self.assertEqual(self.start_lon.text, '3.4')
+        self.assertEqual(self.end_lat.text, '30.2')
+        self.assertEqual(self.end_lon.text, '45.433')
 
 if __name__ == '__main__':
     unittest.main()
