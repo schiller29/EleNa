23636e7b4f8bef06b53f8356ca76b7394e9390ee
Mike Schiller <mschiller@mschiller.local>
2017-12-14 16:29:51 -0500

    more test cases, testing the error messages, plus more validation errors

--- qa/gui/test_ui_validation.py
+++ qa/gui/test_ui_validation.py
@@ -4,6 +4,12 @@ from common import (chromedriver,
 from selenium import webdriver
 from selenium.common.exceptions import ElementNotVisibleException
 import unittest
+from time import sleep
+
+START_LATITUDE = 'Start Latitude'
+START_LONGTITUDE = 'Start Longitude'
+END_LATITUDE = 'End Latitude'
+END_LONGTITUDE = 'End Longitude'
 
 
 class TestStringMethods(unittest.TestCase):
@@ -45,6 +51,7 @@ class TestStringMethods(unittest.TestCase):
         self.max_elevation = get_elem_by_selector(self.elena, '#maximize')
         self.min_elevation = get_elem_by_selector(self.elena, '#minimize')
         self.limit = get_elem_by_selector(self.elena, '#limit')
+        self.endMap = get_elem_by_selector(self.elena, '#endMap')
 
     def scroll_to_bottom(self, top=False):
         if top:
@@ -109,6 +116,159 @@ class TestStringMethods(unittest.TestCase):
         self.assertTrue(wait_for_elem(self.modal, appear=True))
         self.exit_modal()
 
+    def test_latitude_over_max(self):
+        self.start_lat.send_keys('91')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('30.2')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+        self.start_lat.send_keys('49')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('30.2')
+        self.end_lat.send_keys('99')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+
+    def test_latitude_under_min(self):
+        self.start_lat.send_keys('-91')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('30.2')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+        self.start_lat.send_keys('-1000')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('30.2')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+
+    def test_longitude_over_max(self):
+        self.start_lat.send_keys('-65')
+        self.start_lon.send_keys('181')
+        self.end_lon.send_keys('30.2')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+        self.start_lat.send_keys('-65')
+        self.start_lon.send_keys('2000')
+        self.end_lon.send_keys('1000')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+
+    def test_longtitude_under_min(self):
+        self.start_lat.send_keys('-46')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('-181')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+        self.start_lat.send_keys('-46')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('-465783')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+
+    def test_error_message_correct_start_lat(self):
+        self.start_lat.send_keys('-91')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('-134')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        sleep(.5)
+        errorText = get_elem_by_selector(self.elena, '#errorText').text
+        self.assertTrue(START_LATITUDE in errorText)
+        self.exit_modal()
+
+    def test_error_message_correct_end_lat(self):
+        self.start_lat.send_keys('-90')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('-134')
+        self.end_lat.send_keys('7000')
+        self.submit()
+        sleep(.5)
+        errorText = get_elem_by_selector(self.elena, '#errorText').text
+        self.assertTrue(END_LATITUDE in errorText)
+        self.exit_modal()
+
+    def test_error_message_correct_start_lon(self):
+        self.start_lat.send_keys('-90')
+        self.start_lon.send_keys('-190')
+        self.end_lon.send_keys('-134')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        sleep(.5)
+        errorText = get_elem_by_selector(self.elena, '#errorText').text
+        self.assertTrue(START_LONGTITUDE in errorText)
+        self.exit_modal()
+
+    def test_error_message_correct_end_lon(self):
+        self.start_lat.send_keys('-91')
+        self.start_lon.send_keys('3.4')
+        self.end_lon.send_keys('-191')
+        self.end_lat.send_keys('45.433')
+        self.submit()
+        sleep(.5)
+        errorText = get_elem_by_selector(self.elena, '#errorText').text
+        self.assertTrue(END_LONGTITUDE in errorText)
+        self.exit_modal()
+
+    def test_error_message_correct_multiple(self):
+        self.start_lat.send_keys('-91')
+        self.start_lon.send_keys('1299')
+        self.end_lon.send_keys('49292')
+        self.end_lat.send_keys('null')
+        self.submit()
+        sleep(.5)
+        errorText = get_elem_by_selector(self.elena, '#errorText').text
+        self.assertTrue(START_LATITUDE in errorText)
+        self.assertTrue(START_LONGTITUDE in errorText)
+        self.assertTrue(END_LATITUDE in errorText)
+        self.assertTrue(END_LONGTITUDE in errorText)
+        self.exit_modal()
+
+    def test_invalid_limit_in_error_message(self):
+        self.limit.clear()
+        self.limit.send_keys('101')
+        self.submit()
+        sleep(.5)
+        errorText = get_elem_by_selector(self.elena, '#errorText').text
+        self.assertTrue('Total Distance Limit' in errorText)
+        self.exit_modal()
+
+    def test_invalid_limit(self):
+        self.limit.clear()
+        self.limit.send_keys('101')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+        self.limit.clear()
+        self.limit.send_keys('-1')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+        self.limit.clear()
+        self.limit.send_keys('null')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+        self.limit.clear()
+        self.limit.send_keys('true')
+        self.submit()
+        self.assertTrue(wait_for_elem(self.modal, appear=True))
+        self.exit_modal()
+
     def test_valid_decimal(self):
         self.start_lat.send_keys('20.0')
         self.start_lon.send_keys('3.4')
@@ -143,6 +303,7 @@ class TestStringMethods(unittest.TestCase):
         self.limit.clear()
         self.limit.send_keys('10')
         self.submit()
+        self.assertTrue(self.endMap.is_displayed())
 
 
 if __name__ == '__main__':
